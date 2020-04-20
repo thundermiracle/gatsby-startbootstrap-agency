07b2989acc58049829aefa20f7a116094ad388a6
thundermiracle <cissoliu@gmail.com>
2020-04-20 20:37:35 +0900

    add SEO support

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,27 @@ import About from "views/About";
 import Team from "views/Team";
 import Contact from "views/Contact";
 import Footer from "views/Footer";
+import SEO from "components/SEO";
 import "utils/fixFontAwesome";
 import "../style/main.scss";
 
 function IndexPage() {
   return (
     <>
+      <SEO
+        title="Main"
+        keywords={[
+          "gatsby",
+          "gatsbyjs",
+          "landing page",
+          "landing",
+          "i18n",
+          "eslint",
+          "bootstrap",
+          "startbootstrap-agency",
+        ]}
+        description="gatsby version of startbootstrap-agency with i18n support"
+      />
       <Navbar />
       <Top />
       <Services />
