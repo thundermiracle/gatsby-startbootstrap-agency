abf21fb4b3ce99205b243d03cb97d2391674ceea
thundermiracle <cissoliu@gmail.com>
2020-04-30 09:26:33 +0900

    add aria-label to CircleIcon

--- src/components/CircleIcon.jsx
+++ src/components/CircleIcon.jsx
@@ -5,7 +5,13 @@ import Icon from "components/Icon";
 import "./CircleIcon.scss";
 
 const CircleIcon = ({ href, iconName }) => (
-  <a className="circle-icon" href={href} target="_blank" rel="noopener noreferrer">
+  <a
+    className="circle-icon"
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label={iconName}
+  >
     <Icon iconName={iconName} />
   </a>
 );
