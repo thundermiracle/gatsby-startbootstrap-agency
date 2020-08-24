af4340e6a975a2cce8758dc300e8965a115f8c58
thundermiracle <cissoliu@gmail.com>
2020-08-24 21:07:53 +0900

    Revert "use typeface to replace gatsby-plugin-prefetch-google-fonts"
    This reverts commit f1600ba65cebad2b7f7e708f03f7d5f5483f1f00.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-import "typeface-droid-serif";
-import "typeface-kaushan-script";
-import "typeface-montserrat";
-import "typeface-roboto-slab";
